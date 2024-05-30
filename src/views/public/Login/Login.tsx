d8183384dfbb0d0c3f83f8f2474bd2dab6603874
leliasgomer <leliasgomer@gmail.com>
2024-05-30 18:04:03 -0500

    create a hook for login

--- src/views/public/Login/Login.tsx
+++ src/views/public/Login/Login.tsx
@@ -1,37 +1,11 @@
 import { Button } from '@/components/ui/Button'
 import { Input } from '@/components/ui/Input'
 import logo from '@/img/Spotify_Primary_Logo_RGB_White.png'
-import { logIn } from '@/services/logIn'
-import { useUserInfo } from '@/store/userInfo'
+import useLogin from './hooks/useLogin'
 import '@fontsource-variable/outfit'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'sonner'
-import { z } from 'zod'
 
-const schema = z.object({
-  email: z.string().email({ message: 'Debe ser un email valido' }).max(100),
-  password: z.string().min(8, { message: 'Debe ser de 8 caracteres' })
-})
-type FormFields = z.infer<typeof schema>
 export default function Login () {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({
-    resolver: zodResolver(schema)
-  })
-  const setUserInfo = useUserInfo((state) => state.setUser)
-  const navigate = useNavigate()
-  function sendData (data: FormFields) {
-    toast.promise(logIn(data), {
-      loading: 'Cargando...',
-      success: (info) => {
-        setUserInfo(info)
-        navigate('/dashboard')
-        return 'Sesión Iniciada'
-      },
-      error: 'Error al iniciar sesión'
-    })
-  }
+  const { handleSubmit, register, errors, sendData, navigate } = useLogin()
   return (
     <main className="w-full min-h-screen bg-black grid place-content-center text-white">
       <form onSubmit={handleSubmit(sendData)} className="bg-[#242424] w-[400px] p-4 rounded-lg flex flex-col gap-4">
---
+++ src/views/public/Login/hooks/useLogin.tsx
@@ -0,0 +1,39 @@
+import { logIn } from '@/services/logIn'
+import { useUserInfo } from '@/store/userInfo'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const schema = z.object({
+  email: z.string().email({ message: 'Debe ser un email valido' }).max(100),
+  password: z.string().min(8, { message: 'Debe ser de 8 caracteres' })
+})
+type FormFields = z.infer<typeof schema>
+export default function useLogin () {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({
+    resolver: zodResolver(schema)
+  })
+  const setUserInfo = useUserInfo((state) => state.setUser)
+  const navigate = useNavigate()
+  function sendData (data: FormFields) {
+    toast.promise(logIn(data), {
+      loading: 'Cargando...',
+      success: (info) => {
+        setUserInfo(info)
+        navigate('/dashboard')
+        return 'Sesión Iniciada'
+      },
+      error: 'Error al iniciar sesión'
+    })
+  }
+
+  return {
+    errors,
+    register,
+    handleSubmit,
+    sendData,
+    navigate
+  }
+}
