06610ed65218b31596ba34345d6117a44e0ca904
leliasgomer <leliasgomer@gmail.com>
2024-06-01 11:03:46 -0500

    create and Slider from shdcn

---
+++ src/components/ui/Slider.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import * as SliderPrimitive from '@radix-ui/react-slider'
+import '../css/slider.css'
+
+import { cn } from '@/libs/utils'
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative flex w-full touch-none select-none items-center thumb',
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="group/item bg-blackA7 bg-[#444444] relative grow rounded-full h-[4px]">
+      <SliderPrimitive.Range className="range group-hover/item:bg-green-600 duration-100 absolute bg-white hover:bg-green-600 rounded-full h-full" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb className="thumb-circle block h-3 w-3 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+  </SliderPrimitive.Root>
+))
+Slider.displayName = SliderPrimitive.Root.displayName
+
+export { Slider }
