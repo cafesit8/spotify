9f3e9d4bc4d9e663221fb5e11986b2078959a081
leliasgomer <leliasgomer@gmail.com>
2024-06-01 14:16:14 -0500

    add time icon

--- src/icons/icons.tsx
+++ src/icons/icons.tsx
@@ -179,3 +179,9 @@ export function DownloadIcon ({ className }: { className?: string }) {
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 3v12" /><path d="M16 11l-4 4l-4 -4" /><path d="M3 12a9 9 0 0 0 18 0" /></svg>
   )
 }
+
+export function TimeIcon ({ className }: { className?: string }) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 12l3 2" /><path d="M12 7v5" /></svg>
+  )
+}
