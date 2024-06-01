13f971ba3047bd61578597d0a0a424027ddf0ad7
leliasgomer <leliasgomer@gmail.com>
2024-06-01 11:03:57 -0500

    change format time

--- src/services/formaCurrentTime.ts
+++ src/services/formaCurrentTime.ts
@@ -2,7 +2,7 @@ export function formatTime (currentTime: number) {
   if (Number.isNaN(currentTime)) return '00:00'
   const minutes = Math.floor(currentTime / 60)
   const seconds = Math.floor(currentTime % 60)
-  const formattedMinutes = String(minutes).padStart(2, '0')
+  const formattedMinutes = String(minutes).padStart(1, '0')
   const formattedSeconds = String(seconds).padStart(2, '0')
   return `${formattedMinutes}:${formattedSeconds}`
 }
