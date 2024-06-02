c546ff582a98e6580535e305910a78c672b5f7e3
leliasgomer <leliasgomer@gmail.com>
2024-06-02 17:18:11 -0500

    make smaller the realease date

--- src/views/layouts/components/Drawer/components/Header.tsx
+++ src/views/layouts/components/Drawer/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header ({ currentSong }: { currentSong: Music | null }) 
       <div className='flex flex-col justify-end'>
         <h3 className='text-7xl font-semibold'>{currentSong?.name.toLocaleUpperCase()}</h3>
         <h4 className='text-3xl font-semibold'>{currentSong?.artist}</h4>
-        <h4 className='text-3xl text-white/70'>{currentSong?.realease_date && convertDate(currentSong?.realease_date)}</h4>
+        <h4 className='text-lg text-white/70'>{currentSong?.realease_date && convertDate(currentSong?.realease_date)}</h4>
       </div>
     </header>
   )
