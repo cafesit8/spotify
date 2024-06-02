d1a758ec3cc0f4f0c5b9413abf71cd31ca9fb5d1
leliasgomer <leliasgomer@gmail.com>
2024-06-02 17:17:13 -0500

    add play and play button at the drawer

--- src/views/layouts/components/Drawer/Drawer.tsx
+++ src/views/layouts/components/Drawer/Drawer.tsx
@@ -6,18 +6,22 @@ import { usePalette } from 'react-palette'
 import Header from './components/Header'
 import Table from './components/Table'
 
-export default function DrawerContainer ({ playing }: { playing: boolean }) {
+export default function DrawerContainer () {
   const currentSong = useCurrentSong(state => state.currentSong)
+  const { setPlaying, playing } = useCurrentSong()
   const { data } = usePalette(currentSong?.song_cover.url ?? '#8a8b8b')
   const background = data.lightMuted ?? '#8a8b8b'
 
+  function handleClick () {
+    setPlaying(!playing)
+  }
   return (
     <DrawerContent style={{ background: `linear-gradient(180deg, ${background} 0%, #141414 37%)` }} className='w-full h-svh rounded-none text-white border-none'>
       <main className='w-[1300px] m-auto h-full p-3 flex flex-col gap-6 overflow-y-auto'>
         <Header currentSong={currentSong} />
         <section className='flex gap-6 items-center'>
           <ToolTip text={`${playing ? 'Pausar' : 'Reproducir'}`}>
-            <button className='bg-green-500 p-4 rounded-full'>
+            <button onClick={handleClick} className='bg-green-500 p-4 rounded-full'>
               {!playing ? <PlayIcon className='text-black' /> : <PauseIcon className='text-black' />}
             </button>
           </ToolTip>
