31a1dcc5b5c8b155d3b66a478031585c3c843eaf
leliasgomer <leliasgomer@gmail.com>
2024-06-02 17:16:59 -0500

    add playing boolean for global state

--- src/store/currentSong.ts
+++ src/store/currentSong.ts
@@ -3,13 +3,17 @@ import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
 type CurrentSong = {
-  currentSong: null | Music
-  setCurrentSong: (song: any) => void
-}
+  currentSong: null | Music;
+  setCurrentSong: (song: any) => void;
+  playing: boolean;
+  setPlaying: (value: boolean) => void;
+};
 
 export const useCurrentSong = create(persist<CurrentSong>((set) => ({
   currentSong: null,
-  setCurrentSong: (song: any) => set({ currentSong: song })
+  setCurrentSong: (song: any) => set({ currentSong: song }),
+  playing: false,
+  setPlaying: (value) => set({ playing: value })
 }), {
   name: 'currentSong'
 }))
