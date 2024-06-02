21afe6a90e6ad41c560bb38292a82930edffa4fd
leliasgomer <leliasgomer@gmail.com>
2024-06-02 17:18:52 -0500

    delete some props

--- src/views/layouts/components/Player/Player.tsx
+++ src/views/layouts/components/Player/Player.tsx
@@ -4,12 +4,12 @@ import { Info } from './components/Info'
 import { Volume } from './components/Volume'
 
 export default function Player () {
-  const { audioRef, currentSong, handleMuted, handlePlay, handleVolume, currentTime, muted, playing, volume, volumeStep, repeatPlayList, handleRepeat, nextSong, prevSong } = usePlayer()
+  const { audioRef, currentSong, handleMuted, handleVolume, currentTime, playing, volume, volumeStep, repeatPlayList, handleRepeat, nextSong, prevSong } = usePlayer()
   return (
     <footer className='[grid-area:footer] footer bg-[#141414] rounded-xl flex justify-between p-3 gap-3'>
       <Info info={currentSong!} />
-      <Controls audioRef={audioRef} nextSong={nextSong} prevSong={prevSong} repeatPlayList={repeatPlayList} handleRepeat={handleRepeat} currentTime={currentTime} handlePlay={handlePlay} info={currentSong} playing={playing} />
-      <Volume handleMuted={handleMuted} playing={playing} handleVolume={handleVolume} muted={muted} volume={volume} volumeStep={volumeStep} />
+      <Controls audioRef={audioRef} nextSong={nextSong} prevSong={prevSong} repeatPlayList={repeatPlayList} handleRepeat={handleRepeat} currentTime={currentTime} info={currentSong} playing={playing} />
+      <Volume audioRef={audioRef} handleMuted={handleMuted} handleVolume={handleVolume} volume={volume} volumeStep={volumeStep} />
     </footer>
   )
 }
