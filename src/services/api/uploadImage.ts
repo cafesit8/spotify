1c442ecd7e077bd83b00fcc9856355b1b737f613
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:27:32 -0500

    use api url

--- src/services/api/uploadImage.ts
+++ src/services/api/uploadImage.ts
@@ -1,6 +1,8 @@
+import { CLOUDINARY_URL } from '@/config'
+
 export async function uploadImage (formData: FormData, type: string = 'image') {
   try {
-    const res = await fetch(`https://api.cloudinary.com/v1_1/deeezka8t/${type}/upload`, {
+    const res = await fetch(`${CLOUDINARY_URL}/${type}/upload`, {
       method: 'POST',
       body: formData
     })
