31980b443a0356e4e73898ef6459f582af0be5ee
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:29:56 -0500

    create skeleton

---
+++ src/views/layouts/components/PlayList/components/Skeleton.tsx
@@ -0,0 +1,13 @@
+export default function Skeleton () {
+  return (
+    <div className='flex flex-col gap-2'>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+      <div className='w-full h-14 animate-pulse bg-[#222222] rounded-lg'></div>
+    </div>
+  )
+}
