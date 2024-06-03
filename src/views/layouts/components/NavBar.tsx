f311759b03e0f013c73fceb8b7f86d6036df5ae8
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:28:00 -0500

    improve the navbar

--- src/views/layouts/components/NavBar.tsx
+++ src/views/layouts/components/NavBar.tsx
@@ -1,18 +1,39 @@
 import { Button } from '@/components/ui/Button'
 import useNavBar from './hooks/useNavBar'
 import TooltipButton from '@/components/ui/TooltipButton'
+import { useNavigate } from 'react-router-dom'
+import { LeftIcon, RightIcon, UserIcon } from '@/icons/icons'
+import { ToolTip } from '@/components/ui/ToolTip'
 
 export default function NavBar () {
-  const { handleLogOut, userInfo, navigate } = useNavBar()
+  const { handleLogOut, userInfo } = useNavBar()
+  const navigate = useNavigate()
   return (
     <nav className='flex justify-between items-center'>
-      <span className='font-normal text-2xl'>{userInfo?.name}, Bienvenido a Spotify 🙌</span>
+      <div className='flex items-center gap-2'>
+        <ToolTip text='Atrás'>
+          <button onClick={() => navigate(-1)} className='text-white flex items-center bg-[#292929] rounded-full p-1'>
+            <LeftIcon className='w-5 h-5' />
+          </button>
+        </ToolTip>
+        <ToolTip text='Adelante'>
+          <button onClick={() => navigate(+1)} className='text-white flex items-center bg-[#292929] rounded-full p-1'>
+            <RightIcon className='w-5 h-5' />
+          </button>
+        </ToolTip>
+        <span className='font-normal text-xl'>{userInfo?.name}, Bienvenido a Spotify 🙌</span>
+      </div>
       <div className='flex gap-3 relative'>
-        <Button onClick={() => navigate('/dashboard/upload-song')} className='rounded-full px-5'>Subir Canción</Button>
+        <Button onClick={() => navigate('/upload-song')} className='rounded-full px-5'>Subir Canción</Button>
         <TooltipButton className='border w-9 h-9 rounded-full overflow-hidden' tooltiName={userInfo?.username} name={<img className='w-full h-full object-cover' src={userInfo?.photo_profile.url} alt="" />}>
-          <div className='hover:bg-[#303030] text-start p-2 text-sm'>Cuenta</div>
-          <div className='hover:bg-[#303030] text-start p-2 text-sm'>Opciones</div>
-          <div onClick={handleLogOut} className='hover:bg-[#303030] text-start p-2 text-sm'>Salir</div>
+          <div className='flex flex-col'>
+            <button onClick={() => navigate('/my-account')} className='hover:bg-[#303030] text-start p-2 font-normal text-sm flex gap-2'>
+              <UserIcon className='w-5 h-5' />
+              <span>Cuenta</span>
+            </button>
+            <button className='hover:bg-[#303030] text-start p-2 text-sm'>Opciones</button>
+            <button onClick={handleLogOut} className='hover:bg-[#303030] text-start p-2 text-sm'>Salir</button>
+          </div>
         </TooltipButton>
       </div>
     </nav>
