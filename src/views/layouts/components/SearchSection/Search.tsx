adbc5c2d5cf6e25357601de87e895e863acbee9b
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:30:57 -0500

    change navlink

--- src/views/layouts/components/SearchSection/Search.tsx
+++ src/views/layouts/components/SearchSection/Search.tsx
@@ -6,7 +6,7 @@ export default function SearchItem () {
     <div className='[grid-area:search] bg-[#141414] rounded-xl p-5 flex items-center'>
       <ul className='flex flex-col gap-4'>
         <li><NavLink to='/dashboard' className={({ isActive }) => isActive ? 'text-white flex gap-4 font-semibold' : 'text-white/70 flex gap-4 font-semibold'}><HouseIcon /> Inicio</NavLink></li>
-        <li><NavLink to='/dashboard/search' className={({ isActive }) => isActive ? 'text-white flex gap-4 font-semibold' : 'text-white/70 flex gap-4 font-semibold'}><SearchIcon /> Buscar</NavLink></li>
+        <li><NavLink to='/search' className={({ isActive }) => isActive ? 'text-white flex gap-4 font-semibold' : 'text-white/70 flex gap-4 font-semibold'}><SearchIcon /> Buscar</NavLink></li>
       </ul>
     </div>
   )
