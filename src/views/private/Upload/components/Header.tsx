cd66870342d6d26eb2b3e109f198f354e7d0764b
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:31:53 -0500

    create header

---
+++ src/views/private/Upload/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header () {
+  return (
+    <header>
+      <h3 className="text-5xl font-semibold">Comparte Tu Canción</h3>
+      <p className="text-white/80 text-balance">Ahora tienes la oportunidad de compartir con los demás tus gustos musicales y las canciones que te gusten, estos serán publicados para que los demás puedan verlos, escuchar y disfrutar de tus canciones favoritas.</p>
+    </header>
+  )
+}
