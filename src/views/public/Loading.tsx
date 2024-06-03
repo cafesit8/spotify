6fec8ff06d0f7b5d3cac2f5d501257ac2868c415
leliasgomer <leliasgomer@gmail.com>
2024-06-02 23:32:17 -0500

    add and option to transparent loading

--- src/views/public/Loading.tsx
+++ src/views/public/Loading.tsx
@@ -1,8 +1,8 @@
 import gif from '@/img/spotify-loading-unscreen.gif'
 
-export default function Loading () {
+export default function Loading ({ transparent = false }: { transparent?: boolean }) {
   return (
-    <div className="w-full min-h-svh bg-[#141414] grid place-content-center">
+    <div className={`${transparent ? 'bg-transparent' : 'bg-[#141414]'} w-full min-h-svh grid place-content-center`}>
       <img className='w-[450px]' src={gif} alt="" />
     </div>
   )
