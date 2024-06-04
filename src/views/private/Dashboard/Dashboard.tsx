5a1aac69c8b0a141dade0ed6339a595790e50685
leliasgomer <leliasgomer@gmail.com>
2024-06-03 19:32:53 -0500

    import lazy

--- src/views/private/Dashboard/Dashboard.tsx
+++ src/views/private/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
+import { Suspense, lazy } from 'react'
 import Header from './Components/Header'
-import AviciiSection from './Sections/Avicii/AviciiSection'
+import Loading from '@/views/public/Loading'
+const AviciiSection = lazy(() => import('./Sections/Avicii/AviciiSection'))
 
 export default function Dashboard () {
   return (
     <section className='relative z-10 w-full h-full overflow-y-auto flex flex-col gap-7'>
       <Header />
-      <AviciiSection />
+      <Suspense fallback={<Loading transparent />}>
+        <AviciiSection />
+      </Suspense>
     </section>
   )
 }
