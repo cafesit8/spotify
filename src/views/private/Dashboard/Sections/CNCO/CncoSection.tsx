d54e74d6259377f4fc5d6d127257c47ae9eb8544
leliasgomer <leliasgomer@gmail.com>
2024-06-03 22:56:44 -0500

    add cnco and spiderman section

---
+++ src/views/private/Dashboard/Sections/CNCO/CncoSection.tsx
@@ -0,0 +1,7 @@
+import PlayList from '../../Components/PlayList'
+
+export default function CncoSection () {
+  return (
+    <PlayList url='https://api.jsonbin.io/v3/b/665e77d7ad19ca34f873e066' subtitle='Algunos de sus éxitos, CNCO' />
+  )
+}
---
+++ src/views/private/Dashboard/Sections/Spiderman/Spiderman.tsx
@@ -0,0 +1,7 @@
+import PlayList from '../../Components/PlayList'
+
+export default function SpidermanSection () {
+  return (
+    <PlayList url='https://api.jsonbin.io/v3/b/665e8ccfe41b4d34e4fe2a64' subtitle='El universo de Spiderman' />
+  )
+}
