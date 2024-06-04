8179874a62d8a74d9682136603f70eb39cdbf1e5
leliasgomer <leliasgomer@gmail.com>
2024-06-03 23:46:45 -0500

    import lazy components

--- src/views/layouts/DashboardLayout.tsx
+++ src/views/layouts/DashboardLayout.tsx
@@ -1,20 +1,29 @@
 import '@fontsource-variable/outfit'
 import './styles.css'
-import PlayList from './components/PlayList/PlayList'
-import { useState } from 'react'
-import SearchItem from './components/SearchSection/Search'
-import Player from './components/Player/Player'
-import Main from './components/Main/Main'
+import { Suspense, lazy, useState } from 'react'
+import Loading from '../public/Loading'
+const SearchItem = lazy(() => import('./components/SearchSection/Search'))
+const Player = lazy(() => import('./components/Player/Player'))
+const PlayList = lazy(() => import('./components/PlayList/PlayList'))
+const Main = lazy(() => import('./components/Main/Main'))
 
 export default function DashboardLayout () {
   const [collapse, setCollapse] = useState(false)
   const handleCollapse = () => setCollapse(prev => !prev)
   return (
     <div className={`app bg-black grid text-white gap-2 p-4 ${collapse ? 'grid-cols-collapse' : 'grid-cols-no-collapse'}`}>
-      <SearchItem />
-      <PlayList collapse={collapse} handleCollapse={handleCollapse} />
-      <Main />
-      <Player />
+      <Suspense fallback={<Loading transparent />}>
+        <SearchItem />
+      </Suspense>
+      <Suspense fallback={<Loading transparent />}>
+        <PlayList collapse={collapse} handleCollapse={handleCollapse} />
+      </Suspense>
+      <Suspense fallback={<Loading transparent />}>
+        <Main />
+      </Suspense>
+      <Suspense fallback={<Loading transparent />}>
+        <Player />
+      </Suspense>
     </div>
   )
 }
