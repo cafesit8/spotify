c0da8a7f4bea2b75243e448a1635fa6af5c82d94
leliasgomer <leliasgomer@gmail.com>
2024-06-04 00:15:07 -0500

    use lazy import

--- src/views/layouts/components/Drawer/Drawer.tsx
+++ src/views/layouts/components/Drawer/Drawer.tsx
@@ -1,21 +1,28 @@
 import { DrawerContent } from '@/components/ui/Drawer'
 import { useCurrentSong } from '@/store/currentSong'
 import { usePalette } from 'react-palette'
-import Header from './components/Header'
-import Table from './components/Table'
-import Buttons from './components/Buttons'
+import { Suspense, lazy } from 'react'
+const Header = lazy(() => import('./components/Header'))
+const Table = lazy(() => import('./components/Table'))
+const Buttons = lazy(() => import('./components/Buttons'))
 
 export default function DrawerContainer () {
   const currentSong = useCurrentSong(state => state.currentSong)
   const { data } = usePalette(currentSong?.song_cover.url ?? '#8a8b8b')
-  const background = data.lightMuted ?? '#8a8b8b'
+  const background = data.darkMuted ?? '#8a8b8b'
 
   return (
     <DrawerContent style={{ background: `linear-gradient(180deg, ${background} 0%, #141414 37%)` }} className='w-full h-svh rounded-none text-white border-none'>
       <main className='w-[1300px] m-auto h-full p-3 flex flex-col gap-6 overflow-y-auto'>
-        <Header currentSong={currentSong} />
-        <Buttons />
-        <Table />
+        <Suspense fallback={null}>
+          <Header />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Buttons />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Table />
+        </Suspense>
       </main>
     </DrawerContent>
   )
