e3f395669e233e2494b69e7e7f1b6162902afe4b
leliasgomer <leliasgomer@gmail.com>
2024-06-03 22:55:41 -0500

    create and change the sections

--- src/views/private/Dashboard/Dashboard.tsx
+++ src/views/private/Dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { Suspense, lazy } from 'react'
 import Header from './Components/Header'
 import Loading from '@/views/public/Loading'
+const CncoSection = lazy(() => import('./Sections/CNCO/CncoSection'))
+const SpidermanSection = lazy(() => import('./Sections/Spiderman/Spiderman'))
 const AviciiSection = lazy(() => import('./Sections/Avicii/AviciiSection'))
 
 export default function Dashboard () {
@@ -8,7 +10,13 @@ export default function Dashboard () {
     <section className='relative z-10 w-full h-full overflow-y-auto flex flex-col gap-7'>
       <Header />
       <Suspense fallback={<Loading transparent />}>
+        <SpidermanSection />
+      </Suspense>
+      <Suspense fallback={<Loading transparent />}>
         <AviciiSection />
+      </Suspense>
+      <Suspense fallback={<Loading transparent />}>
+        <CncoSection />
       </Suspense>
     </section>
   )
