fd22d8c855ca0aab4ce33cb0b3259faa64094a9f
leliasgomer <leliasgomer@gmail.com>
2024-06-04 00:20:00 -0500

    change gradient color

--- src/views/layouts/components/Drawer/Drawer.tsx
+++ src/views/layouts/components/Drawer/Drawer.tsx
@@ -9,7 +9,7 @@ const Buttons = lazy(() => import('./components/Buttons'))
 export default function DrawerContainer () {
   const currentSong = useCurrentSong(state => state.currentSong)
   const { data } = usePalette(currentSong?.song_cover.url ?? '#8a8b8b')
-  const background = data.darkMuted ?? '#8a8b8b'
+  const background = data.lightVibrant ?? '#8a8b8b'
 
   return (
     <DrawerContent style={{ background: `linear-gradient(180deg, ${background} 0%, #141414 37%)` }} className='w-full h-svh rounded-none text-white border-none'>
