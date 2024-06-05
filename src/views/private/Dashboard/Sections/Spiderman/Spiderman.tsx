86daef8bb1fe34e030809506814d07f4cee55848
leliasgomer <leliasgomer@gmail.com>
2024-06-04 19:43:07 -0500

    change subtitle

--- src/views/private/Dashboard/Sections/Spiderman/Spiderman.tsx
+++ src/views/private/Dashboard/Sections/Spiderman/Spiderman.tsx
@@ -2,6 +2,6 @@ import PlayList from '../../Components/PlayList'
 
 export default function SpidermanSection () {
   return (
-    <PlayList url='https://api.jsonbin.io/v3/b/665e8ccfe41b4d34e4fe2a64' subtitle='El universo de Spiderman' />
+    <PlayList url='https://api.jsonbin.io/v3/b/665e8ccfe41b4d34e4fe2a64' subtitle='El universo de Spider-Man' />
   )
 }
