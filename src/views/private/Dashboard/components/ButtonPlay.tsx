df58f10c9eb3b41f478df551f69383308a04aea3
leliasgomer <leliasgomer@gmail.com>
2024-06-10 17:47:55 -0500

    add use Callback

--- src/views/private/Dashboard/components/ButtonPlay.tsx
+++ src/views/private/Dashboard/components/ButtonPlay.tsx
@@ -2,6 +2,7 @@ import { PauseIcon, PlayIcon } from '@/icons/icons'
 import { useCurrentMusicInfo } from '@/store/currentPlayList'
 import { useCurrentSong } from '@/store/currentSong'
 import { Music } from '@/types/musicList'
+import { useCallback } from 'react'
 
 export default function ButtonPlay ({ song }: { song: Music }) {
   const setCurrentSong = useCurrentSong(state => state.setCurrentSong)
@@ -9,7 +10,7 @@ export default function ButtonPlay ({ song }: { song: Music }) {
   const { currentSong, setPlaying, playing } = useCurrentSong(state => state)
   const isPlaying = currentSong?.id === song.id
 
-  function handleClick () {
+  const handleClick = useCallback(() => {
     if (isPlaying) {
       setPlaying(!playing)
       return
@@ -17,7 +18,7 @@ export default function ButtonPlay ({ song }: { song: Music }) {
     setCurrentSong(song)
     setCurrentMusic(song)
     setPlaying(true)
-  }
+  }, [song, isPlaying, playing, setCurrentSong, setCurrentMusic, setPlaying])
   return (
     <button onClick={handleClick} aria-label='play' className="lg:block hidden card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition">
       {isPlaying && playing ? <PauseIcon className='text-black' /> : <PlayIcon className='text-black' />}
