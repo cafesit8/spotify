bd80fdad1ee838c402e0bd07033e55799bed869a
leliasgomer <leliasgomer@gmail.com>
2024-06-20 18:15:56 -0500

    change configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import MillionLint from '@million/lint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), MillionLint.vite()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
