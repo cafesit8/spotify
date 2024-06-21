8a02e2c1bcfb31a3a90a2dbf8b8dbaf53546baa5
leliasgomer <leliasgomer@gmail.com>
2024-06-20 19:39:28 -0500

    validate

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedRoute ({ children }: { children: ReactNode }) {
-  const user = JSON.parse(localStorage.getItem('userInfo') || '{}')
+  const user = JSON.parse(localStorage.getItem('userInfo'))
 
   if (user != null) {
     if (user.access_token == null) {
