6fd0bfc337ea9d35e58e5e70c14fd5a61a3dd215
Elias Gomer Limaymanta Melgarejo <elimaymanta@zegel.edu.pe>
2025-04-19 18:27:12 -0500

    ✨ change rule for protected route for guest

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,8 +1,13 @@
+import { useUserInfo } from '@/store/userInfo'
 import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedRoute ({ children }: { children: ReactNode }) {
   const user = localStorage.getItem('userInfo')
+  const userInfo = useUserInfo((state) => state.user)
+
+  if (!userInfo?.username) return children
+
   if (user) {
     const parsedUser = JSON.parse(user)
     if (parsedUser.state.access_token) {
